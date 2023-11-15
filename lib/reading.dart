35bed2f01a6bca0a1ec880003dd94991e60364be
EASY-Y <duddms235@naver.com>
2023-11-15 17:01:24 +0900

    수정

--- lib/reading.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-// 다이어리 열람 페이지 
-// 1) 텍스트 (Text)
-// 2) 사진 (imageView)
-// 3) 음악플레이어
-// 4) 감정 이모티콘
-// 5) 수정 버튼 (작성 시점으로부터 24시간만 존재하도록)
