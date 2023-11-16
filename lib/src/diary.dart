26e60b8578fd6cd024abb18b2bdd4b28eefeb770
sieun-Github <sieun6205@naver.com>
2023-11-16 15:54:11 +0900

    text 추가

--- lib/src/diary.dart
+++ lib/src/diary.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
-class Diary extends StatelessWidget {
+class Diary extends StatefulWidget {
   const Diary({super.key});
 
   @override
+  State<Diary> createState() => _DiaryState();
+}
+
+class _DiaryState extends State<Diary> {
+  final TextEditingController _textEditingController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Diary 작성'),
-        backgroundColor: Color(0xFFCCFFFF),
+      body: Center(
+        child: Column(
+          children: [
+            Text('${DateTime.now()}'),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                controller: _textEditingController,
+                maxLength: 140, // 여러 줄
+                decoration: const InputDecoration(
+                  border: InputBorder.none, // 테두리 없음
+                  labelText: '', // 라벨 없음
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Center(child: Text('오늘 하루를 마무리 해보세요.')),
     );
   }
 }
