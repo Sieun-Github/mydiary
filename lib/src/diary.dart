60d4c30a057056ad5ad97b10f3cf112d294dfebe
sieun-Github <sieun6205@naver.com>
2023-11-20 15:02:12 +0900

    뒤로가기 버튼 추가

--- lib/src/diary.dart
+++ lib/src/diary.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Diary extends StatefulWidget {
-  const Diary({super.key});
+  const Diary({Key? key}) : super(key: key);
 
   @override
   State<Diary> createState() => _DiaryState();
@@ -15,6 +16,15 @@ class _DiaryState extends State<Diary> {
       body: Center(
         child: Column(
           children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  GoRouter.of(context).go('/');
+                },
+              ),
+            ),
             Text('${DateTime.now()}'),
             Padding(
               padding: const EdgeInsets.all(16.0),
@@ -22,10 +32,17 @@ class _DiaryState extends State<Diary> {
                 controller: _textEditingController,
                 maxLength: 140, // 여러 줄
                 decoration: const InputDecoration(
-                  border: InputBorder.none, // 테두리 없음
-                  labelText: '', // 라벨 없음
+                  border: OutlineInputBorder(),
+                  labelText: '일기 작성',
                 ),
               ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                String diaryText = _textEditingController.text;
+                print('Diary Text Submitted: $diaryText');
+              },
+              child: const Text('작성'),
             ),
           ],
         ),
