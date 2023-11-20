6301e3130f7f6fc549079d7ef690531dab0d4e92
sieun-Github <sieun6205@naver.com>
2023-11-20 15:28:19 +0900

    뒤로가기 경고창 추가

--- lib/src/diary.dart
+++ lib/src/diary.dart
@@ -1,16 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 
-class Diary extends StatelessWidget {
-  const Diary({super.key});
+class Diary extends StatefulWidget {
+  const Diary({Key? key}) : super(key: key);
 
   @override
+  State<Diary> createState() => _DiaryState();
+}
+
+class _DiaryState extends State<Diary> {
+  final TextEditingController _textEditingController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
+    String formattedDate = DateFormat('yyyy. MM. dd.').format(DateTime.now());
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Diary 작성'),
-        backgroundColor: Color(0xFFCCFFFF),
+      body: Center(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  _showAlertDialog();
+                  //GoRouter.of(context).go('/');
+                },
+              ),
+            ),
+            Text(formattedDate),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                controller: _textEditingController,
+                maxLength: 150, // 여러 줄
+                maxLines: 8,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '일기 작성',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                String diaryText = _textEditingController.text;
+                print('Diary Text Submitted: $diaryText');
+              },
+              child: const Text('작성'),
+            ),
+          ],
+        ),
       ),
-      body: Center(child: Text('오늘 하루를 마무리 해보세요.')),
+    );
+  }
+
+  Future<void> _showAlertDialog() async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('경고'),
+          content: const Text('정말 뒤로 가시겠습니까? 작성 중인 내용이 저장되지 않을 수 있습니다.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the alert dialog
+              },
+              child: const Text('취소'),
+            ),
+            TextButton(
+              onPressed: () {
+                GoRouter.of(context).go('/'); // Navigate back to the home page
+              },
+              child: const Text('확인'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
